b0836e97e028e0f38a3d9f4ed119f757986f1a8e
kroften <sskozhemyakin2006@gmail.com>
2025-03-25 23:15:40 +0300

    fix nvim-treeSHITTER not fucking working on windows

--- init.lua
+++ init.lua
@@ -1029,5 +1029,8 @@ require('lazy').setup({
   },
 })
 
+-- GOVNO
+require('nvim-treesitter.install').compilers = { 'zig', 'clang', 'clang-cl', 'gcc' }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
